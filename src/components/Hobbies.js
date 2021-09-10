95026083268d660669e7d9200e94708fbe2e0b7d
Aravind N <aravind@aravind.n>
2021-09-10 19:37:02 +0530

    Fixed an import issue

--- src/components/Hobbies.js
+++ src/components/Hobbies.js
@@ -1,6 +1,6 @@
 import { BadgeCheckIcon, ChipIcon } from "@heroicons/react/solid";
 import React from "react";
-import { skills } from "../data";
+import { hobbies } from "../data";
 
 export default function Hobbies() {
   return (
